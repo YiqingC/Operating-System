e1572d29bd7493d807570a80856a15e4315d1d38
Yiqing Cao <cao_yiqing@wheatoncollege.edu>
2015-11-20 03:06:13 -0500

    complete

--- P4/CaoOS4.cpp
+++ P4/CaoOS4.cpp
@@ -1,70 +1,69 @@
+/* Yiqing Cao(Grace)
+ *
+ * Program name: OS4
+ * Purpose: A simulation to implement the first-fit memory allocation scheme.
+ * Input  : Prompts for the name of file that each line containes op, PID and size for a process.
+ * Output : An aligned memory table display the PID, beginning address, ending address and size 
+ *			of any process.
+ *			Number of processes (and their PIDs) that did not fit in memory.
+ *			Total amount of memory in use at the end of the simulation.
+ *			Total amount of memory remaining.
+ * 			Number of contiguous spaces (memory blocks) remaining.
+ *			Average size of remaining memory blocks.
+ */
+
 #include<iostream>
 #include<fstream>
 #include<stdlib.h>
 #include<time.h>
 #include<queue>
 
-
 using namespace std;
 
+// global constant
 const int MAINMEMORYSIZE = 1048576; // 1*1024*1024
 
-struct Process{
-	char op;
-	int pid;
-	int size;
-	int begin;
-	int end;
+// class of all the functions and variables needed for simulation 
+class Simulation{
+public:
+	struct Process{
+		char op;
+		int pid;
+		int size;
+		int begin;
+		int end;
+	};
+	vector<Process> incoming; // vector containes all the processes from the input file
+	vector<Process> JobLeft; // vector stores processes that did not fit in memory
+	int NumOfD;
+	int count;
+	int NumOfJobLeft;
+	int LatestEndPosition;
+	// function that read from the input file and store them into vector
+	void ReadInFile(); 
+	// function that given a pid and a vector then find previous index
+	int FindPid(int, vector<Process>&); 
+	// function that do the simulation
+	void ProcessJob(); 
+	// function that print the output
+	void Print(); 
 };
 
-struct Free{
-	int pid;
-	int begin;
-	int end;
-	int size;
-};
-
-void ReadInFile(string, vector<Process>&, int&);
-int FindPid(int, vector<Process>&);
-void ProcessJob(int, int, vector<Process>&, int, vector<Free>&, vector<Free>&);
 
 int main (){
-	string FileName;
-	vector<Process> incoming;
-	int TotalNumOfJob = 0;
-	int TotalMemoryUsed = 0;
-
-	vector<Free> FreeBlocks;
-	vector<Free> JobLeft;
-
-	cout << "Please enter the file name: " << endl;
-	// cin >> FileName;
-	FileName = "inputOS4.txt";
-	ReadInFile(FileName, incoming, TotalNumOfJob);
-
-
-	// // test read file in
-	// for(int i = 0; i < 31; i++)
-	// {
-	// 	cout << incoming[i].op << "\t" << incoming[i].pid << "\t" << incoming[i].size << "\n";
-	// }
-
-	int VectorSize;
-	VectorSize = incoming.size();
-	ProcessJob(TotalNumOfJob, TotalMemoryUsed, incoming, VectorSize, FreeBlocks, JobLeft);
-
-	int newsize = JobLeft.size();
-	for(int i = 0; i < newsize; i++){
-		cout << JobLeft[i].pid << "\t";
-	}
-	cout << endl;
-
-
+	Simulation FirstFit;
+	FirstFit.ReadInFile();
+	FirstFit.ProcessJob();
+	FirstFit.Print();
 	return 0;
 }
 
 
-void ReadInFile(string FileName, vector<Process>& incoming, int& TotalNumOfJob){
+void Simulation::ReadInFile(){
+	string FileName;
+	cout << "Please enter the file name: " << endl;
+	cin >> FileName;
+
 	ifstream fin;
 	fin.open(FileName.c_str());
 	if(fin.is_open()){ // check if file is open
@@ -74,18 +73,21 @@ void ReadInFile(string FileName, vector<Process>& incoming, int& TotalNumOfJob){
 			if(NewProcess.op != 'Q'){
 				if(NewProcess.op != 'D'){
 					fin >> NewProcess.pid >> NewProcess.size;
-					TotalNumOfJob++;
 				}
 				else{
 					fin >> NewProcess.pid;
 					NewProcess.size = 0;
+					NewProcess.begin = 0;
+					NewProcess.end = 0;
 				}
 			}
 			else{	
-				NewProcess.pid = 0;
+				NewProcess.pid = -1;
 				NewProcess.size = 0;
+				NewProcess.begin = 0;
+				NewProcess.end = 0;
 			}
-			incoming.push_back(NewProcess);
+			incoming.push_back(NewProcess); // push process info into vector
 		}
 	}
 	else{
@@ -93,8 +95,8 @@ void ReadInFile(string FileName, vector<Process>& incoming, int& TotalNumOfJob){
 	}
 }
 
-int FindPid(int Pid, vector<Process>& incoming){
-	bool found = 0;
+int Simulation::FindPid(int Pid, vector<Process>& incoming){
+	bool found = 0; // boolean to indicate whether found or not
 	int index = -1;
 	while(!found){
 		index++;
@@ -110,177 +112,118 @@ int FindPid(int Pid, vector<Process>& incoming){
 	}
 }
 
-void ProcessJob(int TotalNumOfJob, int TotalMemoryUsed, vector<Process>& incoming, int VectorSize, vector<Free>& FreeBlocks, vector<Free>& JobLeft){
-	int NumOfJobLeft = 0;
+
+void Simulation::ProcessJob(){
 	int index;
-	Free NewFreeBlock;
-
-	int NumOfFreeBlocks = 0;
-	int j = 0;
-
-	int LatestEndPosition = 0;
-	int NumOfD = 0;
-	for(int i = 0; i < VectorSize; i++){
-
-		if(incoming[i].op == 'A'){
+	NumOfD = 0;
+	count = 0;
+	NumOfJobLeft = 0;
+	LatestEndPosition = 0;
+	
+	int i = 0;
+	while(incoming[i].op != 'Q'){ // run through all the process until reach the end
+		bool insert = 0; // boolean that check whether the process is allocated or not
+		if(incoming[i].op == 'A'){ // allocate the process into memory
 			if(incoming[i].size <= MAINMEMORYSIZE){
-				if(TotalMemoryUsed + incoming[i].size <= MAINMEMORYSIZE){
-					if(!FreeBlocks.empty()){
-						NumOfFreeBlocks = FreeBlocks.size();
-						j = 0;
-						cout << "before while " << NumOfFreeBlocks << "\t" << j << "\n";
-						while(FreeBlocks[j].size < incoming[i].size && j < NumOfFreeBlocks){
-
-							cout << "search for next free block\n";
-
-							j++;
-							cout << "in while " << j << "\n";
-						}
-						cout << "after while " << NumOfFreeBlocks << "\t" << j << "\n";
-
-
-
-					}
-					if(j != NumOfFreeBlocks){
-						cout << "put in this free block\n";
-						incoming[i].begin = FreeBlocks[j].begin;
-						TotalMemoryUsed += incoming[i].size;
+				int k = 0;
+				while(insert == 0 && k < incoming.size()){
+					if(incoming[k].pid == 0 && incoming[k].size >= incoming[i].size){ // allocate into free block
+						incoming[i].begin = incoming[k].begin;
 						incoming[i].end = incoming[i].begin + incoming[i].size - 1;
-
-						FreeBlocks[j].begin += incoming[i].size;
-						// FreeBlocks[j].end   Stay the same?
-						FreeBlocks[j].size -= incoming[i].size;
-
-						cout << incoming[i].pid << "\t" 
-						<< incoming[i].begin << "\t" 
-						<< incoming[i].end << "\t" 
-						<< incoming[i].size << "\n";
-						cout << "New Free" << "\t" 
-						<< FreeBlocks[j].begin << "\t" 
-						<< FreeBlocks[j].end << "\t" 
-						<< FreeBlocks[j].size << "\n";
-
-						cout << "TotalMemoryUsed now is " << TotalMemoryUsed << "\n";
-
+						incoming[k].begin += incoming[i].size;
+						incoming[k].size -= incoming[i].size;
+						insert = 1;
+						incoming.insert(incoming.begin()+k, incoming[i]);
+						incoming.erase(incoming.begin()+i+1);
 					}
-					else if(!FreeBlocks.empty() && j == NumOfFreeBlocks && TotalMemoryUsed < LatestEndPosition){
-						cout << "free block is smaller than size, put at the end of all\n";
+					k++;
+				}
+				if(insert == 0){ // allocate to end if there are enough spaces
+					if(LatestEndPosition + incoming[i].size <= MAINMEMORYSIZE){
 						incoming[i].begin = LatestEndPosition;
-						TotalMemoryUsed += incoming[i].size;
 						LatestEndPosition += incoming[i].size;
 						incoming[i].end = incoming[i].begin + incoming[i].size - 1;
-
-						cout << incoming[i].pid << "\t" 
-						<< incoming[i].begin << "\t" 
-						<< incoming[i].end << "\t" 
-						<< incoming[i].size << "\n";
-
-						cout << "TotalMemoryUsed now is " << TotalMemoryUsed << "\n";
 					}
 					else{
-						incoming[i].begin = TotalMemoryUsed;
-						TotalMemoryUsed += incoming[i].size;
-						incoming[i].end = TotalMemoryUsed - 1;
-						LatestEndPosition += incoming[i].size;
-
-						cout << incoming[i].pid << "\t" 
-						<< incoming[i].begin << "\t" 
-						<< incoming[i].end << "\t" 
-						<< incoming[i].size << "\n";
-						cout << "TotalMemoryUsed now is " << TotalMemoryUsed << "\n";
-						cout << "LatestEndPosition now is " << LatestEndPosition << "\n";
+						JobLeft.push_back(incoming[i]);
+						NumOfJobLeft++;
+						incoming.erase(incoming.begin() + i);
+						i--;
 					}
-
-				
-
-
-				}
-				else{
-					JobLeft[NumOfJobLeft].pid = incoming[i].pid;
-					JobLeft[NumOfJobLeft].begin = 0;
-					JobLeft[NumOfJobLeft].end = 0;
-					JobLeft[NumOfJobLeft].size = 0;
-					NumOfJobLeft++;
 				}
 			}
 			else{
-				JobLeft[NumOfJobLeft].pid = incoming[i].pid;
-				JobLeft[NumOfJobLeft].begin = 0;
-				JobLeft[NumOfJobLeft].end = 0;
-				JobLeft[NumOfJobLeft].size = 0;
+				JobLeft.push_back(incoming[i]);
 				NumOfJobLeft++;
 			}
 		}
-		else if(incoming[i].op == 'D'){
-			
-			//newadd
-			for(int k = 0; k < incoming[i].pid; k++){
-				if(incoming[k].op == 'D'){
-					NumOfD++;
-				}
-			}
-
-			cout << "NumOfJobLeft now is " << NumOfJobLeft << "\n";
-			
-
-
-
+		else if(incoming[i].op == 'D'){ // deallocate the process
+			NumOfD++;
 			index = FindPid(incoming[i].pid, incoming);
-			// index -= NumOfD;//newadd
 			if(index != -1){
-				cout << "index is " << index << "\n";
-				NewFreeBlock.pid = index + 1 - NumOfD;
-				NewFreeBlock.begin = incoming[index].begin;
-				NewFreeBlock.end = incoming[index].end;
-				NewFreeBlock.size = incoming[index].size;
-				TotalMemoryUsed -= incoming[index].size;
-				incoming[index].size = 0;
-			
-
-				if(!FreeBlocks.empty()){
-					NumOfFreeBlocks = FreeBlocks.size();
-					int j = 0;
-					while(index + 1 - NumOfD > FreeBlocks[j].pid && j < NumOfFreeBlocks && NewFreeBlock.begin > FreeBlocks[j].begin){
-						j++;
-					}
-					FreeBlocks.insert(FreeBlocks.begin() + j, NewFreeBlock);
+				incoming[index].pid = 0;
+				incoming.erase(incoming.begin() + i);
+				i--;
+			}
+			for(int k = 0; k < incoming.size() - NumOfD; k++){
+				//check if there are contiguous spaces then coalescing them
+				if(incoming[k].pid == 0 && incoming[k+1].pid == 0){
+						incoming[k].end = incoming[k+1].end;
+						incoming[k].size += incoming[k+1].size;
+						incoming[k+1].size = 0;
+						incoming.erase(incoming.begin() + (k+1));
+						k--;
+						i--;
+						count++;
 				}
-				else{
-					FreeBlocks.push_back(NewFreeBlock);
+				// check whether a free block still have spaces or not
+				if(incoming[k].pid == 0 && incoming[k].size == 0){
+					incoming.erase(incoming.begin() + (k));
+					i--;
+					count++;
 				}
-
-				// FreeBlocks.push_back(NewFreeBlock);
-				// FreeBlocks.insert(FreeBlocks.begin(), NewFreeBlock);
-
-				cout << "Free\t" 
-				<< NewFreeBlock.pid << "\t"
-				<< NewFreeBlock.begin << "\t"
-				<< NewFreeBlock.end << "\t"
-				<< NewFreeBlock.size << "\n";
-
-				cout << "TotalMemoryUsed now is " << TotalMemoryUsed << "\n";
-
-				int newsize = FreeBlocks.size();
-				for(int i = 0; i < newsize; i++)
-				{
-					cout << FreeBlocks[i].begin << "\t" 
-					<< FreeBlocks[i].end << "\t" 
-					<< FreeBlocks[i].size << "\n";
-				}
-
-
 			}
 		}
-
-
+	i++;
 	}
-
-
 }
 
 
+void Simulation::Print(){
+	cout << "\tMemory Table\n";
+	cout << "PID\tBegin\tEnd\tSize\n"; 
+	for(int i = 0; i <= incoming.size() - count; i++){	
+		if(incoming[i].pid == 0){
+			cout << "Free\t" << incoming[i].begin << "\t" << incoming[i].end << "\t" << incoming[i].size << "\n";
+		}
+		else{
+			cout << incoming[i].pid << "\t" << incoming[i].begin << "\t" << incoming[i].end << "\t" << incoming[i].size << "\n";
+		}
+	}
+	cout << "Free\t" << LatestEndPosition << "\t" << MAINMEMORYSIZE << "\t" << MAINMEMORYSIZE - LatestEndPosition + 1 << "\n";
 
+	int TotalFree = MAINMEMORYSIZE - LatestEndPosition + 1;
+	int NumOfFree = 1;
+	for(int i = 0; i <= incoming.size() - count; i++){
+		if(incoming[i].pid == 0){
+			NumOfFree++;
+			TotalFree += incoming[i].size;
+		}
+	}
+	int TotalMemoryUsed = MAINMEMORYSIZE - TotalFree + 1;
+	float AveSizeOfFree = TotalFree / float(NumOfFree);
 
+	cout << "Number of processes that did not fit in memory: " << NumOfJobLeft << "\n";
+	cout << "PIDs: ";
+	for(int i = 0; i < JobLeft.size(); i++){
+		cout << JobLeft[i].pid << "\t";
+	}
+	cout << "\n";
+	cout << "Total amount of memory in use at the end of the simulation: " << TotalMemoryUsed << "\n";
+	cout << "Total amount of memory remaining: " << TotalFree << "\n";
+	cout << "Number of contiguous spaces (memory blocks) remaining: " << NumOfFree << "\n";
+	cout << "Average size of remaining memory blocks: " << AveSizeOfFree << "\n";
+}
 
 
 
